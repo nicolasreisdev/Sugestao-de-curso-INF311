e3e188f5218443549872a9bbae6ab5ee63f0af4a
Nicolas Reis <nicolas.e.silva@ufv.br>
2025-05-19 09:16:56 -0300

    Atualizações.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,10 @@ android {
 }
 
 dependencies {
-
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
