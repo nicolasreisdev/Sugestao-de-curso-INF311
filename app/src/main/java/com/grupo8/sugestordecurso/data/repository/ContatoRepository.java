7990813f05d83605502bb73783a407da06b88d80
Nicolas Reis <nicolas.e.silva@ufv.br>
2025-05-25 21:44:18 -0300

    Criando repositório para encapsular a classe de comunição API via Retrofit.

--- app/src/main/java/com/grupo8/sugestordecurso/data/repository/ContatoRepository.java
+++ app/src/main/java/com/grupo8/sugestordecurso/data/repository/ContatoRepository.java
@@ -1,4 +1,73 @@
 package com.grupo8.sugestordecurso.data.repository;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.grupo8.sugestordecurso.data.api.APIClient;
+import com.grupo8.sugestordecurso.data.api.APIRubeus;
+import com.grupo8.sugestordecurso.data.models.Contato;
+import com.grupo8.sugestordecurso.data.models.RespostaCadastro;
+import com.grupo8.sugestordecurso.data.models.User;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class ContatoRepository {
+    private APIRubeus rubeus;
+
+    public ContatoRepository() {
+        this.rubeus = APIClient.getClient().create(APIRubeus.class);
+    }
+
+    public LiveData<RespostaCadastro> cadastrarContato(Contato contato) {
+        MutableLiveData<RespostaCadastro> data = new MutableLiveData<>();
+        // Envia chamada
+        rubeus.cadastrarContato(contato).enqueue(new Callback<RespostaCadastro>() {
+            @Override
+            public void onResponse(Call<RespostaCadastro> call, Response<RespostaCadastro> response) {
+                RespostaCadastro resposta = response.body();
+                if (resposta.isSuccess()) { // caso o contato seja cadastrado
+                    data.setValue(resposta);
+                } else { // erro no cadastro
+                    data.setValue(null);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RespostaCadastro> call, Throwable t) {
+                data.setValue(null);
+            }
+        });
+
+        return data; // retorna o LiveData
+    }
+
+    public LiveData<RespostaCadastro> buscarUser(User user){
+        MutableLiveData<RespostaCadastro> data = new MutableLiveData<>();
+        // Envia chamada
+        rubeus.buscarUser(user).enqueue(new Callback<RespostaCadastro>() {
+            @Override
+            public void onResponse(Call<RespostaCadastro> call, Response<RespostaCadastro> response) {
+                RespostaCadastro resposta = response.body();
+                if (resposta.isSuccess()) { // caso o contato seja cadastrado
+                    data.setValue(resposta);
+                } else { // erro no cadastro
+                    data.setValue(null);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RespostaCadastro> call, Throwable t) {
+                data.setValue(null);
+            }
+        });
+
+        return data; // retorna o LiveData
+    }
+
+    public Call<RespostaCadastro> criarRegistro(Contato contato){
+        return rubeus.criarRegistro(contato);
+    }
+
 }
