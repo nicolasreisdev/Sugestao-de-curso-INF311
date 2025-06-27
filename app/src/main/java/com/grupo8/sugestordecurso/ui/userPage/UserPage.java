6e7494edc4f479d61db5f29d1b6642fb6abb8eb4
Júlia Galvão <julia.galvao@ufv.br>
2025-06-27 13:59:12 -0300

    alterações pontuais nas telas com adaptação a diferentes aparelhos

--- app/src/main/java/com/grupo8/sugestordecurso/ui/userPage/UserPage.java
+++ app/src/main/java/com/grupo8/sugestordecurso/ui/userPage/UserPage.java
@@ -8,8 +8,10 @@ import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 import android.view.View;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 
 import com.grupo8.sugestordecurso.R;
@@ -18,6 +20,7 @@ import com.grupo8.sugestordecurso.data.models.BodyAPI.BodySugestor;
 import com.grupo8.sugestordecurso.data.models.Interfaces.SugestoesCallback;
 import com.grupo8.sugestordecurso.data.models.RespostasAPI.RespostaSugestor;
 import com.grupo8.sugestordecurso.data.models.Utils.HalfPieChartView;
+import com.grupo8.sugestordecurso.data.models.Utils.SugestaoTendencias;
 import com.grupo8.sugestordecurso.data.models.Utils.User;
 import com.grupo8.sugestordecurso.data.repository.RequestRepository;
 import com.grupo8.sugestordecurso.ui.base.Base;
@@ -28,13 +31,14 @@ import java.util.ArrayList;
 public class UserPage extends Base {
     User user;
     private HalfPieChartView halfPieChart;
-    private HalfPieChartView halfPieChartTend;
+    private SugestaoTendencias sugestao;
     private TextView textViewCurso1Nome, textViewCurso1Prob, textViewCurso1NomeT, textViewCurso1ProbT;
     private TextView textViewCurso2Nome, textViewCurso2Prob, textViewCurso2NomeT, textViewCurso2ProbT;
     private TextView textViewCurso3Nome, textViewCurso3Prob, textViewCurso3NomeT, textViewCurso3ProbT;
     private View colorCurso1, colorCurso2, colorCurso3;
     private View colorCurso1T, colorCurso2T, colorCurso3T;
     private LoadScreen LoadScreen;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,10 +49,9 @@ public class UserPage extends Base {
         LoadScreen = new LoadScreen(); //inicializa a tela de carregamento para ser usada posteriormente
 
         setupBottomNavigation(R.id.nav_home);
-        user = (User)getIntent().getSerializableExtra("user");
+        user = User.getInstance();
 
         halfPieChart = findViewById(R.id.halfPieChart);
-        //halfPieChartTend = findViewById(R.id.halfPieChartT);
 
         // settar as prbabilidades de cada curso retornado pelo modelo e as cores usadas
         textViewCurso1Nome = findViewById(R.id.textViewCurso1Nome);
@@ -63,7 +66,7 @@ public class UserPage extends Base {
         textViewCurso3Prob = findViewById(R.id.textViewCurso3Prob);
         colorCurso3 = findViewById(R.id.colorCurso3);
 
-        //settar as probabilidades de cada curso sugerido pelas tendencias do mercado e cores usadas
+        //settar probabilidades de cada curso dada a tendencia do mercado
         textViewCurso1NomeT = findViewById(R.id.textViewCurso1NomeT);
         textViewCurso1ProbT = findViewById(R.id.textViewCurso1ProbT);
         colorCurso1T = findViewById(R.id.colorCurso1T);
@@ -79,19 +82,24 @@ public class UserPage extends Base {
         //faz requisição para o modelo e atualiza grafico com as predições
         //requisitaModelo("Saúde");
 
-        //pega dados de tendência do mercado na área escolhida pelo usuário
-        String nomeCurso1T = "Engenharia de Software";
-        float probCurso1T = 0.50f; // 50%
+    }
 
-        String nomeCurso2T = "Ciência de Dados";
-        float probCurso2T = 0.30f; // 30%
+    private void atualizaInformacoesTendencias(String nome1, float p1, String nome2, float p2, String nome3, float p3){
+        // Definir nomes dos cursos
+        textViewCurso1NomeT.setText(nome1 + ":");
+        textViewCurso2NomeT.setText(nome2 + ":");
+        textViewCurso3NomeT.setText(nome3 + ":");
 
-        String nomeCurso3T = "Design UX/UI";
-        float probCurso3T = 0.20f; // 20%
+        // Definir probabilidades em porcentagem
+        textViewCurso1ProbT.setText(String.format("%.0f%%", p1 * 100));
+        textViewCurso2ProbT.setText(String.format("%.0f%%", p2 * 100));
+        textViewCurso3ProbT.setText(String.format("%.0f%%", p3 * 100));
 
-        //atualiza o grafico com esses dados
-        //atualizarInformacoesCursosT(nomeCurso1T, probCurso1T, nomeCurso2T, probCurso2T, nomeCurso3T, probCurso3T);
-
+        // Cores para a legenda
+        int[] cores = {Color.parseColor("#eb3455"), Color.parseColor("#eb34b1"), Color.parseColor("#e30733")};
+        colorCurso1T.setBackgroundColor(cores[0]);
+        colorCurso2T.setBackgroundColor(cores[1]);
+        colorCurso3T.setBackgroundColor(cores[2]);
     }
 
     private void atualizarInformacoesCursos(String nome1, float p1, String nome2, float p2, String nome3, float p3) {
@@ -114,33 +122,6 @@ public class UserPage extends Base {
         // Atualizar o gráfico (View Customizada)
         if (halfPieChart != null) {
             halfPieChart.setProbabilities(p1, p2, p3);
-        }
-
-
-    }
-
-    private void atualizarInformacoesCursosT(String nome1, float p1, String nome2, float p2, String nome3, float p3) {
-        // Definir nomes dos cursos
-        textViewCurso1NomeT.setText(nome1 + ":");
-        textViewCurso2NomeT.setText(nome2 + ":");
-        textViewCurso3NomeT.setText(nome3 + ":");
-
-        // Definir probabilidades em porcentagem
-        textViewCurso1ProbT.setText(String.format("%.0f%%", p1 * 100));
-        textViewCurso2ProbT.setText(String.format("%.0f%%", p2 * 100));
-        textViewCurso3ProbT.setText(String.format("%.0f%%", p3 * 100));
-
-        // Cores para a legenda
-        int[] cores = {Color.parseColor("#FF6384"), Color.parseColor("#36A2EB"), Color.parseColor("#FFCE56")};
-        colorCurso1T.setBackgroundColor(cores[0]);
-        colorCurso2T.setBackgroundColor(cores[1]);
-        colorCurso3T.setBackgroundColor(cores[2]);
-
-        // Atualizar o gráfico (View Customizada)
-        if (halfPieChartTend != null) {
-            halfPieChartTend.setProbabilities(p1, p2, p3);
-            // Se quiser passar as cores da activity para a view:
-            // halfPieChart.setColors(new int[]{cores[0], cores[1], cores[2]}); // Você precisaria criar este método em HalfPieChartView
         }
 
 
@@ -176,23 +157,7 @@ public class UserPage extends Base {
     }
 
     public void requisitaModelo(String area){
-        BodySugestor notas = new BodySugestor();
-
-        User user = User.getInstance();
-
-        notas.setNotaMatematica(user.getNotaMatematica());
-        notas.setNotaPortugues(user.getNotaPortugues());
-        notas.setNotaLiteratura(user.getNotaLiteratura());
-        notas.setNotaRedacao(user.getNotaRedacao());
-        notas.setNotaQuimica(user.getNotaQuimica());
-        notas.setNotaFisica(user.getNotaFisica());
-        notas.setNotaBiologia(user.getNotaBiologia());
-        notas.setNotaFilosofia(user.getNotaFilosofia());
-        notas.setNotaSociologia(user.getNotaSociologia());
-        notas.setNotaGeografia(user.getNotaGeografia());
-        notas.setNotaHistoria(user.getNotaHistoria());
-        notas.setNotaArtes(user.getNotaArtes());
-        notas.setAreaPreferencia(user.getAreaPreferencia());
+        BodySugestor notas = getBodySugestor();
         // atualiza a UI com os dados
         RequestRepository sugestaoRepository = new RequestRepository();
 
@@ -208,9 +173,35 @@ public class UserPage extends Base {
 
                 String nomeCurso3 = response.get(1).getCurso();
                 float probCurso3 = response.get(1).getProbabilidadeAptidao();
+
+//                String nomeCurso1T;
+//                float probCurso1T;
+//
+//                String nomeCurso2T;
+//                float probCurso2T;
+//
+//                String nomeCurso3T;
+//                float probCurso3T;
+//
+//                if(area.equals("Saude")){
+//
+//                } else if(area.equals("Exatas")){
+//
+//                } else if(area.equals("Linguagens")){
+//
+//                } else if(area.equals("Biologicas")){
+//
+//                } else if(area.equals("Humanas")){
+//
+//                } else if(area.equals("Artes")){
+//
+//                } else if(area.equals("Tecnologia")){
+//
+//                }
+
                 atualizarInformacoesCursos(nomeCurso1, probCurso1, nomeCurso2, probCurso2, nomeCurso3, probCurso3);
+                //atualizaInformacoesTendencias(nomeCurso1T,probCurso1T,nomeCurso2T,probCurso2T,nomeCurso3T,probCurso3T);
                 BancoDados.getInstance().salvarPredicao(
-                        user.getInstance(),
                         nomeCurso1, probCurso1,
                         nomeCurso2, probCurso2,
                         nomeCurso3, probCurso3
@@ -224,6 +215,26 @@ public class UserPage extends Base {
             }
         });
 
+    }
+
+    @NonNull
+    private BodySugestor getBodySugestor() {
+        BodySugestor notas = new BodySugestor();
+
+        notas.setNotaMatematica(user.getNotaMatematica());
+        notas.setNotaPortugues(user.getNotaPortugues());
+        notas.setNotaLiteratura(user.getNotaLiteratura());
+        notas.setNotaRedacao(user.getNotaRedacao());
+        notas.setNotaQuimica(user.getNotaQuimica());
+        notas.setNotaFisica(user.getNotaFisica());
+        notas.setNotaBiologia(user.getNotaBiologia());
+        notas.setNotaFilosofia(user.getNotaFilosofia());
+        notas.setNotaSociologia(user.getNotaSociologia());
+        notas.setNotaGeografia(user.getNotaGeografia());
+        notas.setNotaHistoria(user.getNotaHistoria());
+        notas.setNotaArtes(user.getNotaArtes());
+        notas.setAreaPreferencia(user.getAreaPreferencia());
+        return notas;
     }
 
 }
