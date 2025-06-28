cd0d8ae95666cf3190c4e5750b9c9aade6682381
Cecilia <cecicassab@gmail.com>
2025-06-28 10:42:05 -0300

    Finalizando para F3.

--- app/src/main/java/com/grupo8/sugestordecurso/data/api/ModeloSugestor/APIClientSugestor.java
+++ app/src/main/java/com/grupo8/sugestordecurso/data/api/ModeloSugestor/APIClientSugestor.java
@@ -6,7 +6,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class APIClientSugestor {
-    private static final String BASE_URL = "http://192.168.2.108:8000/";
+    private static final String BASE_URL = "http://192.168.225.93:8000/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
